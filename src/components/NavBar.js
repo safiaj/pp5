70601fdf7de294b7abcdfe26729c4f56b4e67df4
safia jama <safiajama@icloud.com>
2025-02-14 23:55:17 +0000

    Add navbar, logo and buttons

---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from '.../assets/logo.png'
+
+const NavBar = () => {
+    return (
+        <Navbar expand="md" fixed="top">
+            <Container>
+                <Navbar.Brand>
+                    <img src={logo} alt="logo" height="45" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default NavBar;
